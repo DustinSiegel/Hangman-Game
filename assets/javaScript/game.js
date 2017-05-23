076a7834cc0f46bcdf388ce62e71ab6c18aaaf41
DustinSiegel <weenzombie@gmail.com>
2017-05-23 00:42:51 -0600

    the game works, counts down, and keeps track of wins and losses

--- assets/javaScript/game.js
+++ assets/javaScript/game.js
@@ -23,7 +23,7 @@ var WORDS = [  // An Array of words to use as solutions to the game.
 		"snickerdoodle",
 		"cowboy",
 		"bebop",
-		"antidistablishmentarianism",
+		"electroencephalographically",
 		"kittycat",
 		"marblemouthed"
 ];
@@ -31,16 +31,22 @@ var WORDS = [  // An Array of words to use as solutions to the game.
 
 var word=""; // a varaible that holds the randomly chosen word
 var answerArray = [];	// fills the answer array with an underscore to represent each letter in the word.
+var countdown;	// A countdown display that shows how many guesses remain
 var wins = 0;	// a variable that represents 0 wins and then increments up
 var losses = 0;	// a variable that represents 0 losses and then increments up
+var html = "<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>";	// takes the wins and losses variables and creates a format that will fit in to the html
+document.querySelector ("#game").innerHTML = html;	// calls the location to display created html
 
-function init(){ 	// a function that randomly picks a word from the array and then displays it as underscores
+function start(){ 	// a function that randomly picks a word from the array and then displays it as underscores
   	word = WORDS[Math.floor(Math.random() * WORDS.length)];	//randomly picks a word from the array
   	answerArray = [];	// The variable that creates a new array for the random answer
   	for (var i = 0; i < word.length; i++) {	// a forLoop that runs a cycle for each letter in the word.
     answerArray[i] = "_";	// fills the answer array with an underscore to represent each letter in the word.
+  	countdown = 10;	// A countdown display that shows how many guesses remain
+  	document.getElementById("countdown").innerHTML = countdown;	
+  	document.getElementById("guesses").innerHTML = " ";
   	}
- 
+
 document.getElementById("answer").innerHTML= answerArray.join(" ");	// puts the word chosen into the html
 document.getElementById("message").innerHTML= "Hey Playa, pick a letter!";  //some popup text that tells the player to make an entry
 
@@ -60,9 +66,9 @@ document.onkeypress = function (e) { // recognises when a key is pressed
 	}
 }
 
-init();
+start();
 
-function guessOne() {	// a function that reacts to a players input
+function guessIt() {	// a function that reacts to a players input
     var guess = document.getElementById("guess").value;  // a variable targets the guess paragraph in html
     var showThisMessage = "";	// a variable that provides an blank place to show the messages
 
@@ -73,10 +79,10 @@ function guessOne() {	// a function that reacts to a players input
 	    for (i = 0; i < word.length; i++) {	// a forloop that cycles through a word in the array and compares it to the user imput
 	        if (word[i] === guess) { 	// if the user imput matches one of the letters in the array
 	            answerArray[i] = guess;	// it regesters a guess as correct and pushes a message
-	            showThisMessage = "Awww Yeahh "+guess+" is one of em'.";	// the message that it displays upon a correct guess
+	            showThisMessage = "Awww Yeahh "+ guess +" is one of em'.";	// the message that it displays upon a correct guess
 	        }
 	    }
-	    
+
 	    var remaining_letters = answerArray.length;	// sets the remaining unguessed letters to underscors
 	    for (i = 0; i < answerArray.length; i++) { // a forloop that recounts the word and determines the missing letters
 	        if (answerArray[i] !== '_') {	//  an if statement that determins if there are any more blanks left
@@ -85,36 +91,40 @@ function guessOne() {	// a function that reacts to a players input
 	    }
 	    
 	    if (remaining_letters == 0) {	// if statement tha determines that the amount of letters remaining is 0
-	        wins++;		// increases the win counter by 1
 	        showThisMessage = "Nice sluthin' Columbo, didn't think you had it in ya. The word you found is " + word +"!";	// and displays a congraguatory message
 	    }
 	    
-	    if (showThisMessage === "") {	// if the sowthismessage varaible is exactly equal to a wrong letter
+	    if (showThisMessage === "") {	// if the showthismessage varaible is exactly equal to a wrong letter
 	        showThisMessage = guess + "... nope, that ain't it ";	//it shows a message that has the wrong guess plus informs you to try again
+	   		countdown --;
+	   		document.getElementById("countdown").innerHTML = countdown;	
+	    }
+
+	    if (remaining_letters == 0) {
+	    	wins ++;		// increases the win counter by 1
+	    	var html = "<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>";	// takes the wins and losses variables and creates a format that will fit in to the html
+			document.querySelector ("#game").innerHTML = html;	// calls the location to display created html
+	    }
+
+	    if (countdown == 0) {
+	    	// showthismessage = "Awww dag, you lose. The word was " + word + "!";	//distplays a message saying that you have lost and also showing you the solution
+			losses ++;
+	   		var html = "<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>";	// takes the wins and losses variables and creates a format that will fit in to the html
+			document.querySelector ("#game").innerHTML = html;	// calls the location to display created html
+	    	quit();
 	    }
 
 	    document.getElementById("answer").innerHTML = answerArray.join(" ");  // displays a new selection from the array when refreshed 
-	     document.getElementById("guess").value = "";	//clears the text entry field after a guess is processed
+	    document.getElementById("guess").value = "";	//clears the text entry field after a guess is processed
 		}
 		document.getElementById("message").innerHTML = showThisMessage;		//defines the place in the html to display the messages
 	}
 
 	function quit() {	// a function that acts when you press the quit button
-		losses++;	// increases the losses variable incrementally by 1
-		document.getElementById("message").innerHTML = "Awww dag, you loose. The word was "+word+ "!";	//distplays a message saying that you have lost and also showing you the solution
+		document.getElementById("message").innerHTML = "Give up? The word was "+ word + "!";	//distplays a message saying that you have lost and also showing you the solution
 		for (var j = 0; j < word.length; j++) {		// a forloop that fills in the answer array
 			answerArray[j] = word[j];	// making the answer array the same as the randomly selected word
 		}
 
 		document.getElementById("answer").innerHTML = answerArray.join(" ");	// this recognises that the word has been completed and places it in the html
 }
-
-var html = "<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>";	// takes the wins and losses variables and creates a format that will fit in to the html
-document.querySelector ("#game").innerHTML = html;	// calls the location to display created html
-
-// A countdown display that shows how many guesses remain
-// var countdown = 10;
-// document.onkeydown = function() {
-// 	if (countdown > 0).countdown--;
-// 	document.getElementById("countdown").innerHTML = countdown;	
-// }
